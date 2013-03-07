e80e3e6bcee148b8f3247b4450cec308df597379
Yuya Unno <unnonouno@gmail.com>
2013-03-07 20:56:39 +0900

    Support libsvm format

--- easyjubatus.rb
+++ easyjubatus.rb
@@ -35,25 +35,51 @@ def open_bazil_csv(path)
   end
 end
 
+def open_libsvm(path)
+  open(path).each_line do |line|
+    ss = line.split
+    label = ss[0]
+    num_values = []
+    ss.slice(1, ss.length).each do |s|
+      key, value = s.split(":")
+      num_values << [key, value.to_f]
+    end
+
+    yield label, Jubatus::Classifier::Datum.new([], num_values)
+  end
+end
+
+def open_data(type, path, &block)
+  if type == "csv"
+    open_bazil_csv(path, &block)
+  elsif type == "libsvm"
+    open_libsvm(path, &block)
+  else
+    raise "unsupported file type: \"#{type}\""
+  end
+end
+
 class EasyJubatus < Thor
   class_option :host, :aliases => "-h", :desc => "Host name of jubatus server", :type => :string, :default => "127.0.0.1"
   class_option :port, :aliases => "-p", :desc => "Port number of jubatus server", :type => :numeric, :default => 9199
   class_option :name, :aliases => "-n", :desc => "Cluster name of jubatus server", :type => :string, :default => ""
 
   desc "train", "Train Jubatus with given labeled data."
+  method_option :filetype, :aliases => "-t", :desc => "File type (csv|libsvm)", :type => :string, :default => "csv"
   def train(file)
     client = Jubatus::Classifier::Client::Classifier.new(options[:host], options[:port])
-    open_bazil_csv(file) do |label, datum|
+    open_data(options[:filetype], file) do |label, datum|
       client.train(options[:name], [[label, datum]])
     end
   end
 
   desc "eval", "Evaluate Jubatus with given labeled data."
+  method_option :filetype, :aliases => "-t", :desc => "File type (csv|libsvm)", :type => :string, :default => "csv"
   def eval(file)
     client = Jubatus::Classifier::Client::Classifier.new(options[:host], options[:port])
     correct = 0
     total = 0
-    open_bazil_csv(file) do |label, datum|
+    open_data(options[:filetype], file) do |label, datum|
       res = client.classify(options[:name], [datum])[0]
       max = res.max{ |x, y| x[1] <=> y[1]}
       correct += 1 if max and max[0] == label
